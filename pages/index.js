345beed3721aeea8fc96154c9eba85aa7647f31e
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-08-13 20:02:35 -0400

    Created a new Branch, to start developing footer component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './../components/styles.module.scss'
 
 const Index = () => {
     return (
-            <div>
-                <h1>Welcome  a to Next Application</h1>
+            <div className="container">
+                <h1 className="title">Welcome to a Next Application</h1>
             </div>
     );
 }
