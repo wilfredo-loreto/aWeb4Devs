7b83d73a2c3d00a96f641f8511287630ea13ef11
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-08-17 19:28:11 -0400

    ending version of footer. A lot of changes. Almost finished, Not commented already

--- components/Footer.js
+++ components/Footer.js
@@ -10,83 +10,87 @@ export default function Footer() {
         </h2>
         <a href="#" target="_blank">
           <div className={styles.whatsapp}>
-            <img className={styles.footericon}src="/img/whatsapp.svg" />
+            <img className={styles.footericon} src="/img/whatsapp.svg" />
             <span>+58 424-4320635</span>
           </div>
         </a>
         <a href="#" target="_blank">
           <div className={styles.email}>
-            <img className={styles.footericon}src="/img/email.svg" />
-            <span>test@cookiefox.com</span>
+            <img className={styles.footericon} src="/img/email.svg" />
+            <span>
+              contact@<span className={styles.break}> </span>aweb4devs.com
+            </span>
           </div>
         </a>
       </div>
-      <div className={styles.colcontainer}>
-        <h5>Links</h5>
+      <div className={[styles.colcontainer, styles.cont2].join(" ")}>
+        <h5 className={styles.titles}>Links</h5>
         <ul className={styles.linklist}>
           <li>
-            
-              <div className={styles.rowcontainer}>
-                <img src="/img/listarrow.svg" />
-                <span>Articles</span>
-              </div>
-            
+            <div className={styles.rowcontainer}>
+              <img src="/img/listarrow.svg" />
+              <span>Articles</span>
+            </div>
           </li>
           <li>
-            
-              <div className={styles.rowcontainer}>
-                <img src="/img/listarrow.svg" />
-                <span>Front-end</span>
-              </div>
-            
+            <div className={styles.rowcontainer}>
+              <img src="/img/listarrow.svg" />
+              <span>Front-end</span>
+            </div>
           </li>
           <li>
-            
-              <div className={styles.rowcontainer}>
-                <img src="/img/listarrow.svg" />
-                <span>Back-end</span>
-              </div>
-            
+            <div className={styles.rowcontainer}>
+              <img src="/img/listarrow.svg" />
+              <span>Back-end</span>
+            </div>
           </li>
           <li>
-            
-              <div className={styles.rowcontainer}>
-                <img src="/img/listarrow.svg" />
-                <span>About us</span>
-              </div>
-            
+            <div className={styles.rowcontainer}>
+              <img src="/img/listarrow.svg" />
+              <span>About us</span>
+            </div>
           </li>
           <li>
-            
-              <div className={styles.rowcontainer}>
-                <img src="/img/listarrow.svg" />
-                <span>Contact</span>
-              </div>
-            
+            <div className={styles.rowcontainer}>
+              <img src="/img/listarrow.svg" />
+              <span>Contact</span>
+            </div>
           </li>
           <li>
-            
-              <div className={styles.rowcontainer}>
-                <img src="/img/listarrow.svg" />
-                <span>Privacy Policy</span>
-              </div>
-            
+            <div className={styles.rowcontainer}>
+              <img src="/img/listarrow.svg" />
+              <span>Privacy Policy</span>
+            </div>
           </li>
         </ul>
       </div>
 
-      <div className={styles.colcontainer}>
+      <div className={[styles.colcontainer].join(" ")}>
         <form action="">
-          <h5>Contact us</h5>
-          <input type="email" placeholder="Email..." />
-          <input type="text" placeholder="Message..." />
-          <div className="rowcontainer">
-            <p>
-              <span>*</span>
-              By clicking below to send the message, you're agreeing to our
-              <a>privacy policy</a>
-            </p>
-            <button type="submit">Send</button>
+          <h5 className={styles.titles}>Contact us</h5>
+          <div className={styles.fieldscontainer}>
+            <input
+              className={styles.emailfield}
+              type="email"
+              placeholder="Email"
+            />
+            <textarea
+              className={styles.messagefield}
+              placeholder="Message..."
+              rows="5"
+            />
+          </div>
+          <div className={styles.rowcontainer}>
+            <div className={styles.advice}>
+              <p>
+                <span>* </span>
+                By clicking below to send the message, you're agreeing to our
+                <a> privacy policy</a>
+              </p>
+            </div>
+            <div className={styles.sendbutton}>
+              <button type="submit">Send</button>
+            </div>
           </div>
         </form>
       </div>
