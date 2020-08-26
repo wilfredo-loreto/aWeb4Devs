66f19e3a5ce038c1efcfeba8e74d45a844683aa4
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-08-26 15:32:42 -0400

    start developing front end and backend pages

---
+++ components/Concept.js
@@ -0,0 +1,17 @@
+import styles from './Concept.module.scss'
+
+import React, { Component } from "react";
+
+export default class Concept extends Component {
+  render(){
+  return (
+    /* Carousel Cards  */
+    <div className={styles.mainContainer}>
+        <h1 className={styles.title}>{this.props.title}</h1>
+        <p className={styles.text}>{this.props.description}</p>
+        <p className={styles.text}>In the next sections you will find the most used technologies for {this.props.title} web development</p>
+    </div>
+  );
+
+  }
+}
---
+++ pages/technologies.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+import Concept from "../components/Concept"
+
+function Technologies() {
+  return (
+    <Layout>
+      <div>
+        <Concept title="Front-end" description="This is a long description"/>
+
+      </div>
+    </Layout>
+  );
+}
+
+export default Technologies;
