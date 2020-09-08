5e5234111b4d7a1392515e1dfa1a248c433048d4
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-09-07 22:08:01 -0400

    started developing privacy policy

--- components/PrivacyPolicyContent.js
+++ components/PrivacyPolicyContent.js
@@ -5,11 +5,108 @@ import styles from "./PrivacyPolicyContent.module.scss"
 
 function PrivacyPolicyContent() {
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <h3 className={styles.title}>Privacy Policy</h3>
       <span> Last updated September 21, 2020</span>
-      <p className={styles.text}>Thank you for choosing to be part of our community at aWeb4Devs (“Company”, “we”, “us”, or “our”). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy notice, or our practices with regards to your personal information, please contact us at contact@aweb4devs.com.</p>
-      <p className={styles.text}>When you visit our website <Link href="/"> <a> www.aweb4devs.com </a></Link> (the "Website"), and more generally, use any of our services (the "Services", which include the Website), we appreciate that you are trusting us with your personal information. We take your privacy very seriously. In this privacy notice, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy notice that you do not agree with, please discontinue use of our Services immediately. </p>
+      <p className={styles.text}>Thank you for choosing to be part of our community at aWeb4Devs (“Company”, “we”, “us”, or “our”). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy notice, or our practices with regards to your personal information, please contact us at contact@aweb4devs.com.<br/>
+      When you visit our website <Link href="/"> <a> www.aweb4devs.com </a></Link> (the "Website"), and more generally, use any of our services (the "Services", which include the Website), we appreciate that you are trusting us with your personal information. We take your privacy very seriously. In this privacy notice, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy notice that you do not agree with, please discontinue use of our Services immediately.<br/>
+      This privacy notice applies to all information collected through our Services (which, as described above, includes our Website), as well as any related services, sales, marketing or events.<br/> 
+      <b>Please read this privacy notice carefully as it will help you understand what we do with the information that we collect.</b> </p>
+      <h4>TABLE OF CONTENTS</h4>
+      <ol className={styles.orderedList}>
+          <li className={styles.listItem}>WHAT INFORMATION DO WE COLLECT?</li>
+          <li className={styles.listItem}>HOW DO WE USE YOUR INFORMATION?</li>
+          <li className={styles.listItem}>WILL YOUR INFORMATION BE SHARED WITH ANYONE?</li>
+          <li className={styles.listItem}>WHO WILL YOUT INFORMATION BE SHARED WITH?</li>
+          <li className={styles.listItem}>DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</li>
+          <li className={styles.listItem}>HOW LONG DO WE KEEP YOUR INFORMATION?</li>
+          <li className={styles.listItem}>HOW DO WE KEEP YOUR INFORMATION SAFE?</li>
+          <li className={styles.listItem}>WHAT ARE YOUR PRIVACY RIGHTS?</li>
+          <li className={styles.listItem}>CONTROLS FOR DO-NOT TRACK FEATURES</li>
+          <li className={styles.listItem}>DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</li>
+          <li className={styles.listItem}>DO WE MAKE UPDATES TO THIS NOTICE?</li>
+          <li className={styles.listItem}>HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</li>
+      </ol>
+
+      <h5 className={styles.subtitle}>1. WHAT INFORMATION DO WE COLLECT?</h5>
+      <p className={styles.text}>Personal information you disclose to us <br/>
+<i><b>In Short:</b>  We collect information that you provide to us.</i><br/>
+We collect personal information that you voluntarily provide to us when you express an interest in obtaining information about us or our products and Services, when you participate in activities on the Website or otherwise when you contact us.<br/>
+The personal information that we collect depends on the context of your interactions with us and the Website, the choices you make and the products and features you use. The personal information we collect may include the following:<br/>
+Personal Information Provided by You. We collect names; email addresses; and other similar information.<br/>
+All personal information that you provide to us must be true, complete and accurate, and you must notify us of any changes to such personal information.
+ </p>
+    <h6>Information automatically collected</h6>
+      <p className={styles.text}><i>In Short:  Some information — such as your Internet Protocol (IP) address and/or browser and device characteristics — is collected automatically when you visit our Website.</i>
+
+We automatically collect certain information when you visit, use or navigate the Website. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about who and when you use our Website and other technical information. This information is primarily needed to maintain the security and operation of our Website, and for our internal analytics and reporting purposes.
+
+Like many businesses, we also collect information through cookies and similar technologies.  
+
+The information we collect includes:
+ </p>
+ <ul>
+     <li>Log and Usage Data. Log and usage data is service-related, diagnostic usage and performance information our servers automatically collect when you access or use our Website and which we record in log files. Depending on how you interact with us, this log data may include your IP address, device information, browser type and settings and information about your activity in the Website (such as the date/time stamps associated with your usage, pages and files viewed, searches and other actions you take such as which features you use), device event information (such as system activity, error reports (sometimes called 'crash dumps') and hardware settings).</li>
+     <li>Device Data. We collect device data such as information about your computer, phone, tablet or other device you use to access the Website. Depending on the device used, this device data may include information such as your IP address (or proxy server), device application identification numbers, location, browser type, hardware model Internet service provider and/or mobile carrier, operating system configuration information.</li>
+     <li>Location Data. We collect information data such as information about your device's location, which can be either precise or imprecise. How much information we collect depends on the type of settings of the device you use to access the Website. For example, we may use GPS and other technologies to collect geolocation data that tells us your current location (based on your IP address). You can opt out of allowing us to collect this information either by refusing access to the information or by disabling your Locations settings on your device. Note however, if you choose to opt out, you may not be able to use certain aspects of the Services.</li>
+ </ul>
+ <h5 className={styles.subtitle}>2. HOW DO WE USE YOUR INFORMATION?</h5>
+      <p className={styles.text}><i><b>In Short:</b>  We process your information for purposes based on legitimate business interests, the fulfillment of our contract with you, compliance with our legal obligations, and/or your consent.</i><br/>
+      We use personal information collected via our Website for a variety of business purposes described below. We process your personal information for these purposes in reliance on our legitimate business interests, in order to enter into or perform a contract with you, with your consent, and/or for compliance with our legal obligations. We indicate the specific processing grounds we rely on next to each purpose listed below.<br/>
+      We use the information we collect or receive:
+ </p>
+ <ul>
+     <li><b>To send administrative information to you.</b> We may use your personal information to send you product, service and new feature information and/or information about changes to our terms, conditions, and policies.</li>
+     <li><b> To protect our Services. </b>We may use your information as part of our efforts to keep our Website safe and secure (for example, for fraud monitoring and prevention).</li>
+     <li><b>To enforce our terms, conditions and policies for business purposes, to comply with legal and regulatory requirements or in connection with our contract.</b></li>
+     <li><b>To respond to legal requests and prevent harm.</b> If we receive a subpoena or other legal request, we may need to inspect the data we hold to determine how to respond.</li>
+     <li><b>For other business purposes.</b> We may use your information for other business purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our Website, products, marketing and your experience. We may use and store this information in aggregated and anonymized form so that it is not associated with individual end users and does not include personal information. We will not use identifiable personal information without your consent.</li>
+ </ul>
+
+ <h5 className={styles.subtitle}>3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h5>
+      <p className={styles.text}><i> <b>In Short:</b>  We only share information with your consent, to comply with laws, to provide you with services, to protect your rights, or to fulfill business obligations.</i><br/>
+We may process or share your data that we hold based on the following legal basis:<br/>
+ </p>
+ <ul>
+     <li><b>Consent:</b> We may process your data if you have given us specific consent to use your personal information in a specific purpose. </li>
+     <li><b>Legitimate Interests:</b> We may process your data when it is reasonably necessary to achieve our legitimate business interests. </li>
+     <li><b>Performance of a Contract:</b>Where we have entered into a contract with you, we may process your personal information to fulfill the terms of our contract.</li>
+     <li><b>Legal Obligations:</b>We may disclose your information where we are legally required to do so in order to comply with applicable law, governmental requests, a judicial proceeding, court order, or legal process, such as in response to a court order or a subpoena (including in response to public authorities to meet national security or law enforcement requirements).</li>
+     <li><b>Vital Interests:</b> We may disclose your information where we believe it is necessary to investigate, prevent, or take action regarding potential violations of our policies, suspected fraud, situations involving potential threats to the safety of any person and illegal activities, or as evidence in litigation in which we are involved.</li>
+ </ul>
+      <p className={styles.text}>More specifically, we may need to process your data or share your personal information in the following situations: </p>
+      <ul>
+          <li><b>Business Transfers.</b> We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.</li>
+          <li><b>Vendors, Consultants and Other Third-Party Service Providers.</b> We may share your data with third-party vendors, service providers, contractors or agents who perform services for us or on our behalf and require access to such information to do that work. Examples include: payment processing, data analysis, email delivery, hosting services, customer service and marketing efforts. We may allow selected third parties to use tracking technology on the Website, which will enable them to collect data on our behalf about how you interact with our Website over time. This information may be used to, among other things, analyze and track data, determine the popularity of certain content, pages or features, and better understand online activity. Unless described in this notice, we do not share, sell, rent or trade any of your information with third parties for their promotional purposes.</li>
+      </ul>
+      <h5 className={styles.subtitle}>4. WHO WILL YOUR INFORMATION BE SHARED WITH?</h5>
+      <p className={styles.text}><i><b>In Short:</b> We only share information with the following third parties.</i><br/>
+      We only share and disclose your information with the following third parties. We have categorized each party so that you may easily understand the purpose of our data collection and processing practices. If we have processed your data based on your consent and you wish to revoke your consent, please contact us using the contact details provided in the section below titled "HOW CAN YOU CONTACT US ABOUT THIS NOTICE?".
+      <ul>
+          <li><b>Advertising, Direct Marketing, and Lead Generation</b> <br/> Google AdSense</li>
+      </ul>
+      </p>
+
+      <h5 className={styles.subtitle}>5. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</h5>
+      <p className={styles.text}>
+           
+      <i><b>In Short:</b> We may use cookies and other tracking technologies to collect and store your information.</i><br/>
+      We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Notice.
+      </p>
+      <h5 className={styles.subtitle}>6. HOW LONG DO WE KEEP YOUR INFORMATION?</h5>
+      <p className={styles.text}><i><b>In Short:</b> We keep your information for as long as necessary to fulfill the purposes outlined in this privacy notice unless otherwise required by law.</i><br/>
+      We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting or other legal requirements). No purpose in this notice will require us keeping your personal information for longer than 90 days. <br/>
+      When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.</p>
+      <h5 className={styles.subtitle}>7. HOW DO WE KEEP YOUR INFORMATION SAFE?</h5>
+      <p className={styles.text}><i><b>In Short:</b> We aim to protect your personal information through a system of organizational and technical security measures.</i><br/>
+      We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting or other legal requirements). No purpose in this notice will require us keeping your personal information for longer than 90 days. <br/>
+      When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.</p>
+      <p className={styles.text}> </p>
+      <p className={styles.text}> </p>
+      <p className={styles.text}> </p>
+      <p className={styles.text}> </p>
+      <p className={styles.text}> </p>
+      <p className={styles.text}> </p>
       <p className={styles.text}> </p>
     </div>
   );
