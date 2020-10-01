2d9a54501a36a92bd26d38d5d0cb6afbc32d7641
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-09-30 23:23:01 -0400

    Sort and pagination working correctly

--- components/ContentArticles.js
+++ components/ContentArticles.js
@@ -20,153 +20,18 @@ class ContentArticles extends Component {
     this.setState({ pageOfItems: pageOfItems });
   }
 
-  // Select order desplegable animation 
-  showOptions() {
-    var options = document.getElementById("options");
-    var option1 = document.getElementById("option1");
-    var option2 = document.getElementById("option2");
-    var option3 = document.getElementById("option3");
-    var icon = document.getElementById("icon");
-    
-    options.classList.toggle(styles.hide);
-    icon.classList.toggle(styles.iconActive);
-
-    if (option1.classList.item(0) == styles.option) {
-      option1.classList.replace(styles.option, styles.optionHide);
-      option2.classList.replace(styles.option, styles.optionHide);
-      option3.classList.replace(styles.option, styles.optionHide);
-    } else {
-      option1.classList.replace(styles.optionHide, styles.option);
-      option2.classList.replace(styles.optionHide, styles.option);
-      option3.classList.replace(styles.optionHide, styles.option);
-    }
-  }
-
-  // Choise new order for cards
-  newOrder(x) {
-    var aux = true;
-    var result;
-    console.log(this.state.optionSelected);
-   
-    console.log(this.props.articles)
-
-    if (
-      (x == 1 && this.state.optionSelected == "Most recents") ||
-      (x == 2 && this.state.optionSelected == "Technology") ||
-      (x == 3 && this.state.optionSelected == "Visits")
-    ) {
-      aux = false;
-    }
-
-    if (aux) {
   
-      switch (x) {
-        case 1:
-          result = "Most recents";
-          const newOrder = this.props.articles.sort((a,b) => {
-
-              if(b.date.split("T")[0].split("-")[0] - a.date.split("T")[0].split("-")[0] == 0 ){
-
-                if(b.date.split("T")[0].split("-")[1] - a.date.split("T")[0].split("-")[1] == 0 ){
-
-                  return(b.date.split("T")[0].split("-")[2] - a.date.split("T")[0].split("-")[2])
-                }else{
-
-                  return(b.date.split("T")[0].split("-")[1] - a.date.split("T")[0].split("-")[1])
-
-                }
-
-              }else{
-
-                return(b.date.split("T")[0].split("-")[0] - a.date.split("T")[0].split("-")[0])
-
-              }
-      
-          })
-          this.setState({ articles: newOrder })
-          break;
-        case 2:
-          result = "Technology";
-          break;
-        case 3:
-          result = "Visits";
-     
-          const newOrder1 = this.props.articles.sort((a,b) => {
-
-            return (b.visits - a.visits)
-          })
-
-          console.log(this.props.articles)
-
-          this.setState({ articles: newOrder1 })
-          this.setState({ optionSelected: result });
-          this.showOptions()
-          return(
-           
-            Pagination 
-            
-          )
-          break;
-
-          
-      }
-
-      
-    }
-
-  
-
-  }
 
   render(){
   
-   var articles = this.state.articles
+  
     
  
     return (
       /* ContentCards  */
       <div className={styles.globalContainer}>
         {/* Order select */}
-        <div className={styles.order}>
-          <span onClick={() => this.showOptions()} >Sort by:</span>
-          <div className={styles.selected}>
-            <span onClick={() => this.showOptions()} className={styles.showed}>
-              {this.state.optionSelected}
-            </span>
-            <div
-              id="options"
-              className={[styles.options, styles.hide].join(" ")}
-            >
-              <span
-                onClick={() => this.newOrder(1)}
-                id="option1"
-                className={styles.optionHide}
-              >
-                Most recents
-              </span>
-              <span
-                onClick={() => this.newOrder(2)}
-                id="option2"
-                className={styles.optionHide}
-              >
-                Technology
-              </span>
-              <span
-                onClick={() => this.newOrder(3)}
-                id="option3"
-                className={styles.optionHide}
-              >
-                Visits
-              </span>
-            </div>
-          </div>
-          <img
-            id="icon"
-            onClick={() => this.showOptions()}
-            className={styles.icon}
-            src="/icons/optionArros.png"
-          />
-        </div>
+        
 
         {/* Article cards container */}
         <div className={styles.articlesContainer}>
@@ -177,8 +42,8 @@ class ContentArticles extends Component {
            data = {article} />  
          
           ))}
-          <Pagination items={this.state.articles} onChangePage={this.onChangePage} aux={true} />    
         </div>
+        <Pagination items={this.state.articles} onChangePage={this.onChangePage} />    
       </div>
     );
   }
--- components/Pagination.js
+++ components/Pagination.js
@@ -1,157 +1,288 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ContentArticles.module.scss";
 
 const propTypes = {
-    items: PropTypes.array.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    initialPage: PropTypes.number,
-    pageSize: PropTypes.number
-}
+  items: PropTypes.array.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  initialPage: PropTypes.number,
+  pageSize: PropTypes.number,
+};
 
 const defaultProps = {
-    initialPage: 1,
-    pageSize: 3
-}
+  initialPage: 1,
+  pageSize: 3,
+};
 
 class Pagination extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { pager: {} };
+  constructor(props) {
+    super(props);
+    this.state = { pager: {},optionSelected:"Most recents" };
+
+  }
+
+  componentWillMount() {
+    // set page if items array isn't empty
+    if (this.props.items && this.props.items.length) {
+      this.setPage(this.props.initialPage);
     }
+  }
 
-
-    componentWillMount() {
-        // set page if items array isn't empty
-        if (this.props.items && this.props.items.length) {
-            this.setPage(this.props.initialPage);
-        }
-
+  componentDidUpdate(prevProps, prevState) {
+    // reset page if items array has changed
+    if (this.props.items !== prevProps.items) {
+      this.setPage(this.props.initialPage);
     }
-
-    componentDidUpdate(prevProps, prevState) {
-        // reset page if items array has changed
-        if (this.props.items !== prevProps.items) {
-            this.setPage(this.props.initialPage);
-        }
-    }
-
-    setPage(page) {
-        console.log("hola")
-        var { items, pageSize } = this.props;
-        var pager = this.state.pager;
-
-        if (page < 1 || page > pager.totalPages) {
-            return;
-        }
-
-        // get new pager object for specified page
-        pager = this.getPager(items.length, page, pageSize);
-
-        // get new page of items from items array
-        var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
-
-        // update state
-        this.setState({ pager: pager });
-
-        // call change page function in parent component
-        this.props.onChangePage(pageOfItems);
-    }
-
-    getPager(totalItems, currentPage, pageSize) {
-        // default to first page
-        currentPage = currentPage || 1;
-
-        // default page size is 10
-        pageSize = pageSize || 3;
-
-        // calculate total pages
-        var totalPages = Math.ceil(totalItems / pageSize);
-
-        var startPage, endPage;
-        if (totalPages <= 10) {
-            // less than 10 total pages so show all
-            startPage = 1;
-            endPage = totalPages;
-        } else {
-            // more than 10 total pages so calculate start and end pages
-            if (currentPage <= 6) {
-                startPage = 1;
-                endPage = 10;
-            } else if (currentPage + 4 >= totalPages) {
-                startPage = totalPages - 9;
-                endPage = totalPages;
-            } else {
-                startPage = currentPage - 5;
-                endPage = currentPage + 4;
-            }
-        }
-
-        // calculate start and end item indexes
-        var startIndex = (currentPage - 1) * pageSize;
-        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-        // create an array of pages to ng-repeat in the pager control
-        var pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
-
-        // return object with all pager properties required by the view
-        return {
-            totalItems: totalItems,
-            currentPage: currentPage,
-            pageSize: pageSize,
-            totalPages: totalPages,
-            startPage: startPage,
-            endPage: endPage,
-            startIndex: startIndex,
-            endIndex: endIndex,
-            pages: pages
-        };
-    }
-
-    render() {
-        var pager = this.state.pager;
-
-        if (!pager.pages || pager.pages.length <= 1) {
-            // don't display pager if there is only 1 page
-            return null;
-        }   
-
-        if(this.props.aux){
-
-            return (
-
-                <ul className="pagination">
-                    <li className={pager.currentPage === 1 ? 'disabled' : ''}>
-                        <a onClick={() => this.setPage(1)}>First</a>
-                    </li>
-                    <li className={pager.currentPage === 1 ? 'disabled' : ''}>
-                        <a onClick={() => this.setPage(pager.currentPage - 1)}>Previous</a>
-                    </li>
-                    {pager.pages.map((page, index) =>
-                        <li key={index} className={pager.currentPage === page ? 'active' : ''}>
-                            <a onClick={() => this.setPage(page)}>{page}</a>
-                        </li>
-                    )}
-                    <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
-                        <a onClick={() => this.setPage(pager.currentPage + 1)}>Next</a>
-                    </li>
-                    <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
-                        <a onClick={() => this.setPage(pager.totalPages)}>Last</a>
-                    </li>
-                </ul>
-             
-            
-               
+  }
+  // Select order desplegable animation 
+  showOptions() {
+    var options = document.getElementById("options");
+    var option1 = document.getElementById("option1");
+    var option2 = document.getElementById("option2");
+    var icon = document.getElementById("icon");
     
-            );
+    options.classList.toggle(styles.hide);
+    icon.classList.toggle(styles.iconActive);
 
-        }else{
-            console.log("hola")
-                this.setPage(1)
-            return 0
-        }
-
-        
+    if (option1.classList.item(0) == styles.option) {
+      option1.classList.replace(styles.option, styles.optionHide);
+      option2.classList.replace(styles.option, styles.optionHide);
+    } else {
+      option1.classList.replace(styles.optionHide, styles.option);
+      option2.classList.replace(styles.optionHide, styles.option);
     }
+  }
+
+    // Choise new order for cards
+    newOrder(x) {
+        var aux = true;
+        var result;
+        console.log(this.state.optionSelected);
+        
+       
+      
+    
+        if (
+          (x == 1 && this.state.optionSelected == "Most recents") ||
+          (x == 2 && this.state.optionSelected == "Technology")
+        ) {
+          aux = false;
+          this.showOptions()
+        }
+    
+        if (aux) {
+      
+          switch (x) {
+            case 1:
+              result = "Most recents";
+              
+              const newestOrder = this.props.items.sort((a,b) => {
+    
+                  if(b.date.split("T")[0].split("-")[0] - a.date.split("T")[0].split("-")[0] == 0 ){
+    
+                    if(b.date.split("T")[0].split("-")[1] - a.date.split("T")[0].split("-")[1] == 0 ){
+    
+                      return(b.date.split("T")[0].split("-")[2] - a.date.split("T")[0].split("-")[2])
+                    }else{
+    
+                      return(b.date.split("T")[0].split("-")[1] - a.date.split("T")[0].split("-")[1])
+    
+                    }
+    
+                  }else{
+    
+                    return(b.date.split("T")[0].split("-")[0] - a.date.split("T")[0].split("-")[0])
+    
+                  }
+          
+              })
+              this.setState({ articles: newestOrder,optionSelected:result })
+              break;
+            case 2:
+              result = "Visits";
+         
+              const newOrder1 = this.props.items.sort((a,b) => {
+                return (b.visits - a.visits)
+              })
+    
+              console.log(this.props.items)
+              this.setState({ optionSelected: result,articles: newOrder1 });
+            
+             
+              break;
+    
+              
+          }
+          this.showOptions()
+    this.setPage(1)
+            
+        }
+    
+      
+    
+      }
+    
+
+  setPage(page) {
+    console.log("hola");
+    var { items, pageSize } = this.props;
+    var pager = this.state.pager;
+
+    if (page < 1 || page > pager.totalPages) {
+      return;
+    }
+
+    // get new pager object for specified page
+    pager = this.getPager(items.length, page, pageSize);
+
+    // get new page of items from items array
+    var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
+
+    // update state
+    this.setState({ pager: pager });
+
+    // call change page function in parent component
+    this.props.onChangePage(pageOfItems);
+  }
+
+  getPager(totalItems, currentPage, pageSize) {
+    // default to first page
+    currentPage = currentPage || 1;
+
+    // default page size is 10
+    pageSize = pageSize || 3;
+
+    // calculate total pages
+    var totalPages = Math.ceil(totalItems / pageSize);
+
+    var startPage, endPage;
+    if (totalPages <= 10) {
+      // less than 10 total pages so show all
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      // more than 10 total pages so calculate start and end pages
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
+
+    // calculate start and end item indexes
+    var startIndex = (currentPage - 1) * pageSize;
+    var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+    // create an array of pages to ng-repeat in the pager control
+    var pages = [...Array(endPage + 1 - startPage).keys()].map(
+      (i) => startPage + i
+    );
+
+    // return object with all pager properties required by the view
+    return {
+      totalItems: totalItems,
+      currentPage: currentPage,
+      pageSize: pageSize,
+      totalPages: totalPages,
+      startPage: startPage,
+      endPage: endPage,
+      startIndex: startIndex,
+      endIndex: endIndex,
+      pages: pages,
+    };
+  }
+
+  render() {
+    var pager = this.state.pager;
+
+    if (!pager.pages || pager.pages.length <= 1) {
+      // don't display pager if there is only 1 page
+      return null;
+    }
+
+   
+      return (
+        <React.Fragment>
+          <div className={styles.containerOrder}>
+            <div className={styles.order}>
+              <span onClick={() => this.showOptions()}>Sort by:</span>
+              <div className={styles.selected}>
+                <span
+                  onClick={() => this.showOptions()}
+                  className={styles.showed}
+                >
+                  {this.state.optionSelected}
+                </span>
+                <div
+                  id="options"
+                  className={[styles.options, styles.hide].join(" ")}
+                >
+                  <span
+                    onClick={() => this.newOrder(1)}
+                    id="option1"
+                    className={styles.optionHide}
+                  >
+                    Most recents
+                  </span>
+                  <span
+                    onClick={() => this.newOrder(2)}
+                    id="option2"
+                    className={styles.optionHide}
+                  >
+                    Visits
+                  </span>
+                </div>
+              </div>
+              <img
+                id="icon"
+                onClick={() => this.showOptions()}
+                className={styles.icon}
+                src="/icons/optionArros.png"
+              />
+            </div>
+          </div>
+          <div className={styles.containerPagination}>
+            <ul className="pagination">
+              <li className={pager.currentPage === 1 ? "disabled" : ""}>
+                <a onClick={() => this.setPage(1)}>First</a>
+              </li>
+              <li className={pager.currentPage === 1 ? "disabled" : ""}>
+                <a onClick={() => this.setPage(pager.currentPage - 1)}>
+                  Previous
+                </a>
+              </li>
+              {pager.pages.map((page, index) => (
+                <li
+                  key={index}
+                  className={pager.currentPage === page ? "active" : ""}
+                >
+                  <a onClick={() => this.setPage(page)}>{page}</a>
+                </li>
+              ))}
+              <li
+                className={
+                  pager.currentPage === pager.totalPages ? "disabled" : ""
+                }
+              >
+                <a onClick={() => this.setPage(pager.currentPage + 1)}>Next</a>
+              </li>
+              <li
+                className={
+                  pager.currentPage === pager.totalPages ? "disabled" : ""
+                }
+              >
+                <a onClick={() => this.setPage(pager.totalPages)}>Last</a>
+              </li>
+            </ul>
+          </div>
+        </React.Fragment>
+      );
+  }
 }
 
 Pagination.propTypes = propTypes;
