619602f9980891962bfe28113b27fb191e88a76d
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-10-27 19:07:01 -0400

    Developed cookies page and popup

--- components/CookieConsentBanner.js
+++ components/CookieConsentBanner.js
@@ -2,12 +2,12 @@ import styles from "./CookieConsentBanner.module.scss";
 import React, { Component } from "react";
 
 export default class CookieConsentBanner extends Component {
-
+   
   render() {
     return (
         <div id={"container"} onClick={this.hideContainer} className={styles.mainContainer}>
-            <span>We use third party cookies to improve our website. By tapping on a link, on the Accept button, or (otherwise) by continuing to browse , you agreeing to our and our partner's use of cookies and other technologies to process your personal data to personalise your experience and deliver tailored advertising to you. Tap <a id={"disable"} href="#">here to disable cookies</a></span>
-            <button id={"button"}className={styles.button}>Accept</button>
+            <span>We use third party cookies to improve our website. By tapping on a link, on the Accept button, or (otherwise) by continuing to browse , you agreeing to our and our partner's use of cookies and other technologies to process your personal data to personalise your experience and deliver tailored advertising to you. Tap <a id={"disable"} href="" onClick={this.props.stopCookies}>here to disable cookies</a></span>
+            <button onClick={this.props.handleBanner}id={"button"}className={styles.button}>Accept</button>
         </div>
     )
   }
--- components/Layout.js
+++ components/Layout.js
@@ -2,19 +2,52 @@ import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer"
 import CookieConsentBanner from "./CookieConsentBanner";
-function Layout({ children }) {
-  return (
-    <div>
-      <Head>
-        <title>AWeb4Devs</title>
-        <link rel="icon" type="image/png" href="/img/favicon.png"/>
-      </Head>
-      <Header />
-      <div> {children}</div>
-      <CookieConsentBanner/>
-      <Footer />
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class Layout extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      showCookieConsentBanner:false
+    }
+    this.handleBanner = this.handleBanner.bind(this)
+    this.stopCookies= this.stopCookies.bind(this)
+  }
+
+  handleBanner(e){
+    this.setState({showCookieConsentBanner:false})
+  }
+  stopCookies(e){
+    this.setState({showCookieConsentBanner:false})
+    /*CODE HERE TO STOP COOKIES FROM GOOGLE ADSENSE*/
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      if(!localStorage.getItem("cookieBannerDisplayed")){
+        localStorage.setItem("cookieBannerDisplayed","true")
+        this.setState({showCookieConsentBanner:true})
+      }
+    },2000)
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        <Head>
+          <title>AWeb4Devs</title>
+          <link rel="icon" type="image/png" href="/img/favicon.png"/>
+        </Head>
+        <Header />
+        <div> {this.props.children}</div>
+      
+        {this.state.showCookieConsentBanner == true ? <CookieConsentBanner stopCookies={this.stopCookies} handleBanner={this.handleBanner}/>:null}
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default Layout;
+
