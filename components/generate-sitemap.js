86a0506d82f79eb3a0c0cae9abaa33a985d7d93a
Wilfredo Loreto <pipeloreto99@gmail.com>
2021-01-19 16:06:05 -0400

    fixed sitemap first line empty

--- components/generate-sitemap.js
+++ components/generate-sitemap.js
@@ -67,8 +67,7 @@ const axios = require("axios");
       .join("")}
 `;
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
